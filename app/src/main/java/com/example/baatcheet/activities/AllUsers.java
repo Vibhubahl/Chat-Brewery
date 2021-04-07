f2d6095b47cce77ddf4eb90a643068210991ea16
Jatin Singh <jatin.singhgill37@gmail.com>
2021-04-07 12:07:41 +0530

    Test Commit1

--- app/src/main/java/com/example/baatcheet/activities/AllUsers.java
+++ app/src/main/java/com/example/baatcheet/activities/AllUsers.java
@@ -23,7 +23,6 @@ public class AllUsers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_users);
-
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
         FirebaseFirestore.getInstance().collection("users").get().addOnCompleteListener(task -> {
